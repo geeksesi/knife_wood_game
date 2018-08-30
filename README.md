6645e8b34ac8a373fee267e5dd415cee75c77a6f
geeksesi <geeksesi@gmail.com>
2018-08-30 14:31:36 +0430

    a funny Test

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ it's a very vaery simple Game ( now you can't call this a game it's just a test 
 
 make by phaser freamwork and javascript
 
-can u play in geeksesi.github.io/knife_wood_game (i think because now i will try to add this url :D )
+can u play in [knife Wood Game](https://geeksesi.github.io/knife_wood_game)
+
+### made just for fun :D 
--- assets/knife_wood.js
+++ assets/knife_wood.js
@@ -1,10 +1,7 @@
-    window.onload = function() {
+    window.onload = function() 
+    {
 
-        //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-        //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-        //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
-
-        var game = new Phaser.Game("80%", "100%", Phaser.CANVAS, '', { preload: preload, create: create, update: update, resize:onResize });
+        var game = new Phaser.Game(750, 1334, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
         function preload () 
         {
 
@@ -14,53 +11,72 @@
         }
         var wood;
         var knife;
+        var knifeGroup;
+        var canThrow;
+        var knife2;
         function create () 
         {
             game.stage.backgroundColor = '#efefef';
             // game.stage.x = game.world.centerX;
 
-            knife = this.game.add.sprite(game.world.centerX, 0, 'knife');
+            knife = game.add.sprite(game.world.centerX, 100, 'knife');
+
             game.physics.enable(knife, Phaser.Physics.ARCADE);
             wood = game.add.sprite(game.world.centerX, (game.world.centerY + 50), 'my_wood');
             wood.anchor.setTo(0.5, 0.5);
+            wood.depth = 1;
 
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.pageAlignHorizontally = true;
-            this.scale.pageAlignVertically = true;
+
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVertically = true;
+
+            knifeGroup = game.add.group();
+            canThrow = true;
+
+
         }
         function update() 
         {
-            wood.angle += 0.5;
-
-            if (game.input.activePointer.isDown)
+            wood.angle += 2;
+            game.input.onDown.add(knife_shot);
+            key1 = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            key1.onDown.add(knife_shot);
+            if (knife.body.y > 450) 
             {
-                knife_shot();
+                knife.body.velocity.y= 0;
+                knife.body.y = 99;
             }
-            if (knife.body.y > 130) 
+            else if (knife.body.y < 100) 
             {
-                knife.body.velocity.y= -400;
+                knife.body.velocity.y= 0;
+                knife.body.y = 101;
+            }
+
+            var children = knifeGroup.children;
+            for (var i = 0; i < children.length; i++)
+            {
+
+            // rotating the knife
+            children[i].angle += 2;
+
+            // turning knife angle in radians
+            var radians = Phaser.Math.degToRad(children[i].angle + -90);
+
+            // trigonometry to make the knife rotate around target center
+            children[i].x = wood.x + (wood.width / 2) * Math.cos(radians);
+            children[i].y = wood.y + (wood.width / 3) * Math.sin(radians);
+        
             }
         }
         function knife_shot() 
         {
-            console.log("HEllo");
-            knife.body.velocity.y=1000;
-            console.log(knife.body.y);
-
-        }
-
-        function onResize() 
-        {
-            alert("hello"); 
-            //scaling options
-            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            //have the game centered horizontally
-            game.scale.pageAlignHorizontally = true;
-            game.scale.pageAlignVertically = true;
-            //screen size will be set automatically
-            game.scale.setScreenSize(true);
+            knife.body.velocity.y=1500;
+            knife.body.y = 403;
+            knife2 = game.add.sprite(knife.x, 403, "knife");
+            knifeGroup.add(knife2);
+   
 
         }
     };
-        //     access_log /home/geeksesi/public_html/OnlineAdhanPlayer/access.log;
-        // error_log /home/geeksesi/public_html/OnlineAdhanPlayer/error.log;
+
