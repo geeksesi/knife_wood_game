2d1e937a309e965e4afd0d4d3bb0b85469ab09a5
geeksesi <geeksesi@gmail.com>
2018-08-30 15:27:27 +0430

    switch to phaser3 and it's work :lol

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ make by phaser freamwork and javascript
 can u play in [knife Wood Game](https://geeksesi.github.io/knife_wood_game)
 
 ### made just for fun :D 
+
+خب مدیونید فکر کنید که بازی رو خودم ساختم  بعد نسخه دوم این کتابخونه از قابلیت 
+z-index 
+پشتیبانی نکرد و مجبور شدم سوییچ کنم روی نسخه ۳ 
+و تمام کد ها رو از یه بازی آماده کپی کردم :)
--- assets/knife_wood.js
+++ assets/knife_wood.js
@@ -1,82 +1,189 @@
-    window.onload = function() 
+/**
+ *  made by : Mohammad Javad Ghasemy 
+ *  email : geeksesi [at] gmail [dot] com
+ *  github : https://github.com/geeksesi
+ *  game page : https://geeksesi.github.io/knife_wood_game
+ * 
+ *  v0.0.0 fun
+ *
+ *  by phaser 3.1.1.0
+ *
+ *  if you need more info please tell me :D 
+ * 
+ */
+
+
+
+// the game itself
+var game;
+
+// global game options
+var gameOptions = {
+
+    // target rotation speed, in degrees per frame
+    rotationSpeed: 3,
+
+    // knife throwing duration, in milliseconds
+    throwSpeed: 150
+}
+
+
+window.onload = function() 
+{
+
+    // game configuration object
+    var gameConfig = 
     {
 
-        var game = new Phaser.Game(750, 1334, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
-        function preload () 
-        {
+        // render type
+       type: Phaser.CANVAS,
 
-            game.load.image('knife', 'assets/img/knife.png');
-            game.load.image('my_wood', 'assets/img/my_wood.png');
+       // game width, in pixels
+       width: 750,
 
+       // game height, in pixels
+       height: 1334,
+
+       // game background color
+       backgroundColor: 0xefefef,
+
+       // scenes used by the game
+       scene: [playGame]
+    };
+
+    // game constructor
+    game = new Phaser.Game(gameConfig);
+
+    // pure javascript to give focus to the page/frame and scale the game
+    window.focus()
+    resize();
+    window.addEventListener("resize", resize, false);
+}
+
+// PlayGame scene
+class playGame extends Phaser.Scene{
+
+    // constructor
+    constructor(){
+        super("PlayGame");
+    }
+
+    // method to be executed when the scene preloads
+    preload(){
+
+        // loading assets
+        this.load.image("target", "assets/img/my_wood.png");
+        this.load.image("knife", "assets/img/knife.png");
+    }
+
+    // method to be executed once the scene has been created
+    create(){
+
+        // can the player throw a knife? Yes, at the beginning of the game
+        this.canThrow = true;
+
+        // group to store all rotating knives
+        this.knifeGroup = this.add.group();
+
+        // adding the knife
+        this.knife = this.add.sprite(game.config.width / 2, 250, "knife");
+
+        // adding the target
+        this.target = this.add.sprite(game.config.width / 2, game.config.height / 5 * 3, "target");
+
+        // moving the target on front
+        this.target.depth = 1;
+
+        // waiting for player input to throw a knife
+        this.input.on("pointerdown", this.throwKnife, this);
+    }
+
+    // method to throw a knife
+    throwKnife(){
+
+        // can the player throw?
+        if(this.canThrow){
+
+            // player can't throw anymore
+            this.canThrow = false;
+
+            // tween to throw the knife
+            this.tweens.add({
+
+                // adding the knife to tween targets
+                targets: [this.knife],
+
+                // y destination
+                y: this.target.y ,
+
+                // tween duration
+                duration: gameOptions.throwSpeed,
+
+                // callback scope
+                callbackScope: this,
+
+                // function to be executed once the tween has been completed
+                onComplete: function(tween){
+
+                    // player can now throw again
+                    this.canThrow = true;
+
+                    // adding the rotating knife in the same place of the knife just landed on target
+                    var knife = this.add.sprite(this.knife.x, this.knife.y, "knife");
+
+                    // adding the rotating knife to knifeGroup group
+                    this.knifeGroup.add(knife);
+
+                    // bringing back the knife to its starting position
+                    this.knife.y = 250;
+                }
+            });
         }
-        var wood;
-        var knife;
-        var knifeGroup;
-        var canThrow;
-        var knife2;
-        function create () 
-        {
-            game.stage.backgroundColor = '#efefef';
-            // game.stage.x = game.world.centerX;
+    }
 
-            knife = game.add.sprite(game.world.centerX, 100, 'knife');
+    // method to be executed at each frame
+    update(){
 
-            game.physics.enable(knife, Phaser.Physics.ARCADE);
-            wood = game.add.sprite(game.world.centerX, (game.world.centerY + 50), 'my_wood');
-            wood.anchor.setTo(0.5, 0.5);
-            wood.depth = 1;
+        // rotating the target
+        this.target.angle += gameOptions.rotationSpeed;
 
+        // getting an array with all rotating knives
+        var children = this.knifeGroup.getChildren();
 
-            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            game.scale.pageAlignHorizontally = true;
-            game.scale.pageAlignVertically = true;
-
-            knifeGroup = game.add.group();
-            canThrow = true;
-
-
-        }
-        function update() 
-        {
-            wood.angle += 2;
-            game.input.onDown.add(knife_shot);
-            key1 = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-            key1.onDown.add(knife_shot);
-            if (knife.body.y > 450) 
-            {
-                knife.body.velocity.y= 0;
-                knife.body.y = 99;
-            }
-            else if (knife.body.y < 100) 
-            {
-                knife.body.velocity.y= 0;
-                knife.body.y = 101;
-            }
-
-            var children = knifeGroup.children;
-            for (var i = 0; i < children.length; i++)
-            {
+        // looping through rotating knives
+        for (var i = 0; i < children.length; i++){
 
             // rotating the knife
-            children[i].angle += 2;
+            children[i].angle += gameOptions.rotationSpeed;
 
             // turning knife angle in radians
-            var radians = Phaser.Math.degToRad(children[i].angle + -90);
+            var radians = Phaser.Math.DegToRad(children[i].angle + -90);
 
             // trigonometry to make the knife rotate around target center
-            children[i].x = wood.x + (wood.width / 2) * Math.cos(radians);
-            children[i].y = wood.y + (wood.width / 3) * Math.sin(radians);
-        
-            }
+            children[i].x = this.target.x + (this.target.width / 2) * Math.cos(radians);
+            children[i].y = this.target.y + (this.target.width / 2) * Math.sin(radians);
         }
-        function knife_shot() 
-        {
-            knife.body.velocity.y=1500;
-            knife.body.y = 403;
-            knife2 = game.add.sprite(knife.x, 403, "knife");
-            knifeGroup.add(knife2);
-   
 
-        }
-    };
+    }
+}
+
+// pure javascript to scale the game
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
+
+
 
