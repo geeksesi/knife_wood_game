8f80b89a99d955818356ef0bc42e99727cf9a137
geeksesi <geeksesi@gmail.com>
2018-08-31 19:29:24 +0430

    a new idea of homayon

--- README.md
+++ README.md
@@ -7,7 +7,5 @@ can u play in [knife Wood Game](https://geeksesi.github.io/knife_wood_game)
 
 ### made just for fun :D 
 
-خب مدیونید فکر کنید که بازی رو خودم ساختم  بعد نسخه دوم این کتابخونه از قابلیت 
-z-index 
-پشتیبانی نکرد و مجبور شدم سوییچ کنم روی نسخه ۳ 
-و تمام کد ها رو از یه بازی آماده کپی کردم :)
+- javascript programmer : me ( mohammad javad ghasemy)
+- graphic and idea : homayon naseery @Afro_Beast
--- assets/knife_wood.js
+++ assets/knife_wood.js
@@ -1,5 +1,5 @@
 /**
- *  made by : Mohammad Javad Ghasemy 
+ *  made by : Mohammad Javad Ghasemy & homayon naseery
  *  email : geeksesi [at] gmail [dot] com
  *  github : https://github.com/geeksesi
  *  game page : https://geeksesi.github.io/knife_wood_game
@@ -21,10 +21,10 @@ var game;
 var gameOptions = {
 
     // target rotation speed, in degrees per frame
-    rotationSpeed: 3,
+    rotationSpeed: 1,
 
-    // knife throwing duration, in milliseconds
-    throwSpeed: 150
+    // shot throwing duration, in milliseconds
+    throwSpeed: 90
 }
 
 
@@ -45,7 +45,7 @@ window.onload = function()
        height: 1334,
 
        // game background color
-       backgroundColor: 0xefefef,
+       backgroundColor: 0x1d0000,
 
        // scenes used by the game
        scene: [playGame]
@@ -72,34 +72,48 @@ class playGame extends Phaser.Scene{
     preload(){
 
         // loading assets
-        this.load.image("target", "assets/img/my_wood.png");
-        this.load.image("knife", "assets/img/knife.png");
+        this.load.image("target", "assets/img/circle.png");
+        this.load.image("shot", "assets/img/shot.png");
+        this.load.image("path", "assets/img/path.png");
+        // this.load.image("path2", "assets/img/path_trans.png");
     }
 
     // method to be executed once the scene has been created
     create(){
 
-        // can the player throw a knife? Yes, at the beginning of the game
+        // can the player throw a shot? Yes, at the beginning of the game
         this.canThrow = true;
 
         // group to store all rotating knives
-        this.knifeGroup = this.add.group();
+        this.shotGroup = this.add.group();
+        
+        this.pathGroup = this.add.group();
 
-        // adding the knife
-        this.knife = this.add.sprite(game.config.width / 2, 250, "knife");
+        // adding the shot
+        this.shot = this.add.sprite(game.config.width / 2, 250, "shot");
 
         // adding the target
         this.target = this.add.sprite(game.config.width / 2, game.config.height / 5 * 3, "target");
 
         // moving the target on front
-        this.target.depth = 1;
+        // this.pathGroup.depth = 2;
 
-        // waiting for player input to throw a knife
-        this.input.on("pointerdown", this.throwKnife, this);
+        // var path1 = this.add.sprite(game.config.width / 2, (this.target.y - (this.target.width / 2)), "path");
+        this.path1 = this.add.sprite(300, 200, "path");
+        this.path1.depth = 1;
+        this.path2 = this.add.sprite(200, 200, "path");
+        this.path2.depth = 1;
+
+
+
+        // adding the rotating shot to shotGroup group
+
+        // waiting for player input to throw a shot
+        this.input.on("pointerdown", this.throwshot, this);
     }
 
-    // method to throw a knife
-    throwKnife(){
+    // method to throw a shot
+    throwshot(){
 
         // can the player throw?
         if(this.canThrow){
@@ -107,14 +121,14 @@ class playGame extends Phaser.Scene{
             // player can't throw anymore
             this.canThrow = false;
 
-            // tween to throw the knife
+            // tween to throw the shot
             this.tweens.add({
 
-                // adding the knife to tween targets
-                targets: [this.knife],
+                // adding the shot to tween targets
+                targets: [this.shot],
 
                 // y destination
-                y: this.target.y ,
+                y: (this.target.y - (this.target.width / 2)) ,
 
                 // tween duration
                 duration: gameOptions.throwSpeed,
@@ -125,45 +139,63 @@ class playGame extends Phaser.Scene{
                 // function to be executed once the tween has been completed
                 onComplete: function(tween){
 
-                    // player can now throw again
                     this.canThrow = true;
+                    var shot1 = this.add.sprite(this.shot.x, this.shot.y, "shot");
+                    this.shotGroup.add(shot1);
+                    this.shot.y = 250;
 
-                    // adding the rotating knife in the same place of the knife just landed on target
-                    var knife = this.add.sprite(this.knife.x, this.knife.y, "knife");
-
-                    // adding the rotating knife to knifeGroup group
-                    this.knifeGroup.add(knife);
-
-                    // bringing back the knife to its starting position
-                    this.knife.y = 250;
+                    var way_to_path1_x = this.shot.x - this.path1.x; 
+                    var way_to_path1_y = this.path1.y - this.shot.y; 
+                    var way_to_path2_x = this.shot.x - this.path2.x; 
+                    var way_to_path2_y = this.path2.y - this.shot.y;; 
+                    console.log(way_to_path1_x+":::"+way_to_path1_y+"|||"+way_to_path2_x+":::"+way_to_path2_y);
+                    if (way_to_path1_x > -58 && way_to_path1_x < 58 && 200 < way_to_path1_y && way_to_path1_y < 300) 
+                    {
+                        alert("Lose");
+                    }
+                    if (way_to_path2_x > -58 && way_to_path2_x < 58 && 200 < way_to_path2_y && way_to_path2_y < 300) 
+                    {
+                        alert("Lose");
+                    }
                 }
             });
         }
     }
 
-    // method to be executed at each frame
+    // method to be executed at each frame 
     update(){
 
         // rotating the target
         this.target.angle += gameOptions.rotationSpeed;
 
         // getting an array with all rotating knives
-        var children = this.knifeGroup.getChildren();
+        var children_shot = this.shotGroup.getChildren();
 
         // looping through rotating knives
-        for (var i = 0; i < children.length; i++){
+        for (var i = 0; i < children_shot.length; i++)
+        {
 
-            // rotating the knife
-            children[i].angle += gameOptions.rotationSpeed;
+            // rotating the shot
+            children_shot[i].angle += gameOptions.rotationSpeed;
 
-            // turning knife angle in radians
-            var radians = Phaser.Math.DegToRad(children[i].angle + -90);
+            // turning shot angle in radians
+            var radians = Phaser.Math.DegToRad(children_shot[i].angle + -90);
 
-            // trigonometry to make the knife rotate around target center
-            children[i].x = this.target.x + (this.target.width / 2) * Math.cos(radians);
-            children[i].y = this.target.y + (this.target.width / 2) * Math.sin(radians);
+            // trigonometry to make the shot rotate around target center
+            children_shot[i].x = this.target.x + (this.target.width / 2) * Math.cos(radians);
+            children_shot[i].y = this.target.y + (this.target.width / 2) * Math.sin(radians);
         }
+        var children_path = this.pathGroup.getChildren();
+        
+        this.path1.angle += gameOptions.rotationSpeed;
+        this.radians_path_0 = Phaser.Math.DegToRad(this.path1.angle +90);
+        this.path1.x = this.target.x + (this.target.width / 2) * Math.cos(this.radians_path_0);
+        this.path1.y = this.target.y + (this.target.width / 2) * Math.sin(this.radians_path_0);
 
+        this.path2.angle += gameOptions.rotationSpeed;
+        this.radians_path_1 = Phaser.Math.DegToRad(this.path2.angle + -22);
+        this.path2.x = this.target.x + (this.target.width / 2) * Math.cos(this.radians_path_1);
+        this.path2.y = this.target.y + (this.target.width / 2) * Math.sin(this.radians_path_1);
     }
 }
 
